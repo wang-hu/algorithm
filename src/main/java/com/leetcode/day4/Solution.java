1e944c8d64949c4a093e29e6215d17819220fdfa
531510119@qq.com <15151871897@163.com>
2020-03-19 23:40:26 +0800

    Longest palindrome string

---
+++ src/main/java/com/leetcode/day4/Solution.java
@@ -0,0 +1,33 @@
+package com.leetcode.day4;
+
+/**
+ 给定一个包含大写字母和小写字母的字符串，找到通过这些字母构造成的最长的回文串。
+ 在构造过程中，请注意区分大小写。比如 "Aa" 不能当做一个回文字符串。
+
+ 注意:
+ 假设字符串的长度不会超过 1010。
+
+ 示例 1:
+ 输入:
+ "abccccdd"
+ 输出:
+ 7
+
+ 解释:
+ 我们可以构造的最长的回文串是"dccaccd", 它的长度是 7。
+ *
+ * @author wanghu
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int longestPalindrome(String s) {
+
+        return 0;
+    }
+}
